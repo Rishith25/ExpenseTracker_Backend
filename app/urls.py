12ed90e8aac8cfb3c16bf3436974297e500bb79f
Rishith25 <kamishetty.rishith@gmail.com>
2024-04-28 19:28:46 +0530

    urls updated

--- app/urls.py
+++ app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('account/create', AccountView.as_view(), name='account_create'),
     path('account/', AccountView.as_view(), name='account'),
     path('account/<int:pk>/', AccountView.as_view(), name='account update'),
-    path('account/transaction/', TransactionView.as_view(), name='transaction'),
-    path('account/transaction/<int:pk>/', TransactionView.as_view(), name='update and delete'),
+    path('account/transaction', TransactionView.as_view(), name='transaction'),
+    path('account/transaction/<int:pk>', TransactionView.as_view(), name='update and delete'),
     path('account/analytics', AnalyticsDataView.as_view(), name='analytics'),
 ]
